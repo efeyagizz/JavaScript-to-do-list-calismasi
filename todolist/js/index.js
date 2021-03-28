b95ca9607ff65d8adb3d89f47139b6ba8cad3064
efeyagizz <efegoturen@hotmail.com>
2021-03-28 21:15:45 +0300

    first commit

---
+++ todolist/js/index.js
@@ -0,0 +1,60 @@
+// kapatma tusu
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// kapatma tusu
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// "tamamlandi(ustunu cizme)"
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// yeni liste olusturma
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("task").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    $(document).ready(function(){     // bildirim gosterme kismi (hata)
+      $('.error').toast('show');
+    });
+  } else {
+    $(document).ready(function(){
+      $('.success').toast('show');   // bildirim gosterme kismi (basarili)
+    });
+    document.getElementById("list").appendChild(li);
+  }
+  document.getElementById("task").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
